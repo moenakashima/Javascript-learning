c34878e37907ade839ff075d6539eb7c0b87a39e
moenakashima <moenakashima.mn@gmail.com>
2022-12-26 13:45:27 +0900

    if else exercise

--- Conditionals/Conditional Statement/if.js
+++ Conditionals/Conditional Statement/if.js
@@ -3,3 +3,13 @@ let sale = true;
 if (sale){
   console.log('Time to buy!');
 }
+
+// If...Else Statements
+sale = false;
+
+if(sale) {
+  console.log('Time to buy!');
+} else {
+  console.log('Time to wait for a sale.');
+}
+
