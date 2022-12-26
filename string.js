02106758f7be7055ab8b65b8927b685a0e5c8cb9
moenakashima <moenakashima.mn@gmail.com>
2022-12-26 11:58:58 +0900

    String Interpolation exe

--- string.js
+++ string.js
@@ -1,3 +1,8 @@
 // String Concatenation with Variables
 let favoriteAnimal = 'dog';
 console.log('My favorite animal:' + favoriteAnimal); 
+
+// String Interpolation
+const myName = 'Moe';
+let myCity = 'Kyoto';
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
