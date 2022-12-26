0df73c60c6da534060309f7c398231d4581d8299
moenakashima <moenakashima.mn@gmail.com>
2022-12-26 13:52:50 +0900

    ComparisonOperators exe

--- Conditionals/Conditional Statement/if.js
+++ Conditionals/Conditional Statement/if.js
@@ -13,3 +13,12 @@ if(sale) {
   console.log('Time to wait for a sale.');
 }
 
+// Comparison Operators
+let hungerLevel = 7;
+
+if (hungerLevel < 7){
+  console.log('Time to eat!');
+} else {
+  console.log('We can eat later!');
+}
+
