1890f2fc8bb44cd75ef82e6fb2317403835bde29
moenakashima <moenakashima.mn@gmail.com>
2022-12-26 16:54:59 +0900

    default parameters exercise

--- Functions/functions.js
+++ Functions/functions.js
@@ -23,3 +23,10 @@ function getReminder(){
   
   sayThanks('Cole');
   
+// Default Parameters
+  function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
+    console.log(`Remember to buy ${item1}`);
+    console.log(`Remember to buy ${item2}`);
+    console.log(`Remember to buy ${item3}`);
+  }
+  
