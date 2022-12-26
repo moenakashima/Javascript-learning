2443ac27000ee2006923182f4529f33227f97452
moenakashima <moenakashima.mn@gmail.com>
2022-12-26 14:12:13 +0900

    Truthy and Falsy Assignment exe

--- Conditionals/Conditional Statement/Truly and Falsy.js
+++ Conditionals/Conditional Statement/Truly and Falsy.js
@@ -1,3 +1,4 @@
+// Truthy and Falsy
 let wordCount = 2;
 
 if (wordCount) {
@@ -14,3 +15,10 @@ if (favoritePhrase) {
 } else {
   console.log('This string is definitely empty.');
 }
+
+// Truthy and Falsy Assignment
+let tool = 'marker';
+
+let writingUtensil = tool|| 'pen';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
