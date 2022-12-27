be2fa361906b4ab6736f3a08a8291294898843bd
moenakashima <moenakashima.mn@gmail.com>
2022-12-27 13:23:13 +0900

    helper funtcions exercise

--- Functions/functions.js
+++ Functions/functions.js
@@ -37,3 +37,18 @@ function getReminder(){
   
   const numOfMonitors = monitorCount(5,4);
   console.log(numOfMonitors);
+
+
+//  Helper Functions
+  function monitorCount(rows, columns) {
+    return rows * columns;
+  }
+  
+  function costOfMonitors(rows, columns){
+    return monitorCount(rows, columns) * 200;
+  }
+  
+  const totalCost = costOfMonitors(5,4);
+  
+  console.log(totalCost);
+  
