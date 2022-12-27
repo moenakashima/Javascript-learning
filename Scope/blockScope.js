0eed98e374568acea7c68f9a05e231eb4b51eab9
moenakashima <moenakashima.mn@gmail.com>
2022-12-27 17:00:50 +0900

    blockScopeExercise

---
+++ Scope/blockScope.js
@@ -0,0 +1,8 @@
+const logVisibleLightWaves = () => {
+    const lightWaves = 'Moonlight';
+    console.log(lightWaves);
+  };
+  
+  
+  logVisibleLightWaves(); //Prints 'Moonlight'
+  console.log(lightWaves); // throws a RefarenceError
