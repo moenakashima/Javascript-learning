c32587e3a73f570b0167d67a00490bf51cf86ddb
moenakashima <moenakashima.mn@gmail.com>
2022-12-27 17:12:39 +0900

    goodScopingExercise

--- Scope/scopePollution.js
+++ Scope/scopePollution.js
@@ -1,3 +1,4 @@
+// Example of a bad acoping (overwriting a global variable)
 const satellite = 'The Moon';
 const galaxy = 'The Milky Way';
 let stars = 'North Star';
@@ -9,3 +10,18 @@ const callMyNightSky = () => {
 
 console.log(callMyNightSky());
 console.log(stars);
+
+// Exapmple of a good scopling
+const logVisibleLightWaves = () => {
+    let lightWaves = 'Moonlight';
+      let region = 'The Arctic';
+    // Add if statement here:
+    if (region === 'The Arctic'){
+      let lightWaves = 'Northern Lights';
+      console.log(lightWaves);
+    };  
+    console.log(lightWaves);
+  };
+  
+  logVisibleLightWaves();
+  
