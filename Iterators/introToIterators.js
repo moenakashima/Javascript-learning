4977339e80dc5b10459a8c87c89f3503b88a76e8
moenakashima <moenakashima.mn@gmail.com>
2023-01-09 22:34:26 +0900

    [Start]IteratorsExercise

---
+++ Iterators/introToIterators.js
@@ -0,0 +1,17 @@
+
+const numbers = [1, 2, 3, 4, 5];
+
+const squareNumbers = numbers.map(number => {
+  return number * number;
+});
+
+console.log(squareNumbers);
+
+const things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
+
+const onlyNumbers = things.filter(thing => {
+  return typeof thing === 'number';
+});
+
+console.log(onlyNumbers);
+
