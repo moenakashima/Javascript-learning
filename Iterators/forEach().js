cb2dfdbf08decb6a0a52b0e8e8e962554c7edacb
moenakashima <moenakashima.mn@gmail.com>
2023-01-10 22:01:07 +0900

    forEachMethodExercise

---
+++ Iterators/forEach().js
@@ -0,0 +1,8 @@
+const fruits = ["mango", "papaya", "pineapple", "apple"];
+
+// Iterate over fruits below
+
+fruits.forEach(function (fruitsItem) {
+  console.log("I want to eat a " + fruitsItem);
+});
+
