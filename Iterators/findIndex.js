487759ae560f96f2a84e6c69eeb59e277af9d358
moenakashima <moenakashima.mn@gmail.com>
2023-01-11 22:52:45 +0900

    findIndexExercise

---
+++ Iterators/findIndex.js
@@ -0,0 +1,13 @@
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+
+const foundAnimal = animals.findIndex(
+  animal => {
+  return animal === 'elephant';
+  });
+
+const startsWithS = animals.findIndex(
+  animal => {
+    return animal[0] === 's'
+// return animal.chartAt(0) === 's' でもOK
+  }
+)
