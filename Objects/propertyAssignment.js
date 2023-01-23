e708e0e80e21d5855caef64c37c18a26bcc95944
moenakashima <moenakashima.mn@gmail.com>
2023-01-23 22:05:27 +0900

    PropertyAssignmentExercise

---
+++ Objects/propertyAssignment.js
@@ -0,0 +1,9 @@
+let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    homePlanet : 'Earth',
+    color: 'silver',
+    'Secret Mission' : 'Discover life outside of Earth.'
+  };
+  
+  // Write your code below
+  
