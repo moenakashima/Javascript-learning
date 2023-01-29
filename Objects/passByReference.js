6e0f28c2d051cd0f08b2fb49e8bfafbb874db156
moenakashima <moenakashima.mn@gmail.com>
2023-01-29 20:08:44 +0900

    reference exercise

--- Objects/passByReference.js
+++ Objects/passByReference.js
@@ -1,6 +1,17 @@
 let spaceship = {
-    'Fuel Type' : 'Turbo Fuel',
-    homePlanet : 'Earth'
-  };
-  
-  
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth'
+};
+
+let greenEnergy = obj => {
+  obj['Fuel Type'] = 'avocado oil'
+};
+
+let remotelyDisable = obj => {
+  obj.disabled = true
+};
+
+greenEnergy(spaceship);
+remotelyDisable(spaceship);
+
+console.log(spaceship);
