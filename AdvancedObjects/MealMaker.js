981ecd6e72b0df06a059987150e8fdf572ce333a
moenakashima <moenakashima.mn@gmail.com>
2023-02-10 10:07:33 +0900

    task5 complete

--- AdvancedObjects/MealMaker.js
+++ AdvancedObjects/MealMaker.js
@@ -1,13 +1,6 @@
 const menu = {
-    _meal : '',
-    _price : 0
-  };
-  
-  set meal(mealToCheck) {
-    
-  };
-  
-  menu._meal = 10;
-  menu._price = 'hot meal';
-  
-  console.log(menu);
+  _meal : '',
+  _price : 0,
+
+  set meal(mealToCheck) {}
+};
