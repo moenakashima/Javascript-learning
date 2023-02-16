0cfa6eaf330ee14efb20618aedbde88b507b1738
moenakashima <moenakashima.mn@gmail.com>
2023-02-16 15:26:47 +0900

    [Finish]TeamStatusExercise

--- AdvancedObjects/TeamStatus.js
+++ AdvancedObjects/TeamStatus.js
@@ -27,13 +27,26 @@ const team = {
       };
   
       this.players.push(player);
-    }
+    },
+  
+    addGame(newOpponent, newTeamPoints, newOpponentPoints){
+    let game = {
+      opponent: newOpponent,
+      teamPoints: newTeamPoints,
+      opponentPoints: newOpponentPoints
+    };
+  
+    this.games.push(game);
+  }
+  
   };
   
   team.addPlayer('Bugs', 'Bunny', 76);
   
   console.log(team.players);
   
+  team.addGame('Titans', 100, 98);
   
+  console.log(team.games);
   
   
