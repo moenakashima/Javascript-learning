0d28644bb045e277cf1a5e644f1b7f80ffd4fd9e
moenakashima <moenakashima.mn@gmail.com>
2023-02-16 15:18:53 +0900

    TeamStatusAdded

---
+++ AdvancedObjects/TeamStatus.js
@@ -0,0 +1,39 @@
+const team = {
+    _players: [
+      {firstName: 'Ben', lastName: 'Stone', age: 24},
+      {firstName: 'Ken', lastName: 'Abe', age: 40},
+      {firstName: 'Bob', lastName: 'Baur', age: 32}      
+      ],
+  
+    _games: [
+      {opponent: 'Redblue', teamPoints: 3, opponentPoints: 1},
+      {opponent: 'ColleMac', teamPoints: 1, opponentPoints: 1}, 
+      {opponent: 'YellowBeans', teamPoints: 10, opponentPoints: 12}
+    ],
+  
+    get players(){
+      return this._players;
+      },
+  
+    get games(){
+      return this._games;
+    },
+  
+    addPlayer(newFirstName, newLastName, newAge){
+      let player = {
+        firstName: newFirstName,
+        lastName: newLastName,
+        age: newAge
+      };
+  
+      this.players.push(player);
+    }
+  };
+  
+  team.addPlayer('Bugs', 'Bunny', 76);
+  
+  console.log(team.players);
+  
+  
+  
+  
